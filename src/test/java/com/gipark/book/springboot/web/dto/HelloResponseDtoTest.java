3558a5bb8dd49434f683d1bacc0d0f3f2f0d6b14
gipark <gipark181@gmail.com>
2022-08-07 21:14:30 +0900

    Test lombok Function

---
+++ src/test/java/com/gipark/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.gipark.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokFuncTest() {
+        //given
+        String name = "test";
+        int amount = 1300;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
